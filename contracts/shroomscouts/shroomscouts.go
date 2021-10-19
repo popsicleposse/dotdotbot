826f69bb898f666a5c1136b1bc23ce7e5cb5742e
Ryan W <rbrickproductions123@gmail.com>
2021-10-19 15:46:48 -0500

    shroom scouts badge minter

---
+++ contracts/shroomscouts/shroomscouts.go
@@ -0,0 +1,201 @@
+// Code generated - DO NOT EDIT.
+// This file is a generated binding and any manual changes will be lost.
+
+package shroomscouts
+
+import (
+	"errors"
+	"math/big"
+	"strings"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/event"
+)
+
+// Reference imports to suppress errors if they are not otherwise used.
+var (
+	_ = errors.New
+	_ = big.NewInt
+	_ = strings.NewReader
+	_ = ethereum.NotFound
+	_ = bind.Bind
+	_ = common.Big1
+	_ = types.BloomLookup
+	_ = event.NewSubscription
+)
+
+// ShroomscoutsMetaData contains all meta data concerning the Shroomscouts contract.
+var ShroomscoutsMetaData = &bind.MetaData{
+	ABI: "[{\"inputs\":[{\"internalType\":\"uint256\",\"name\":\"amount\",\"type\":\"uint256\"}],\"name\":\"mintToken\",\"outputs\":[],\"stateMutability\":\"nonpayable\",\"type\":\"function\"}]",
+}
+
+// ShroomscoutsABI is the input ABI used to generate the binding from.
+// Deprecated: Use ShroomscoutsMetaData.ABI instead.
+var ShroomscoutsABI = ShroomscoutsMetaData.ABI
+
+// Shroomscouts is an auto generated Go binding around an Ethereum contract.
+type Shroomscouts struct {
+	ShroomscoutsCaller     // Read-only binding to the contract
+	ShroomscoutsTransactor // Write-only binding to the contract
+	ShroomscoutsFilterer   // Log filterer for contract events
+}
+
+// ShroomscoutsCaller is an auto generated read-only Go binding around an Ethereum contract.
+type ShroomscoutsCaller struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ShroomscoutsTransactor is an auto generated write-only Go binding around an Ethereum contract.
+type ShroomscoutsTransactor struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ShroomscoutsFilterer is an auto generated log filtering Go binding around an Ethereum contract events.
+type ShroomscoutsFilterer struct {
+	contract *bind.BoundContract // Generic contract wrapper for the low level calls
+}
+
+// ShroomscoutsSession is an auto generated Go binding around an Ethereum contract,
+// with pre-set call and transact options.
+type ShroomscoutsSession struct {
+	Contract     *Shroomscouts     // Generic contract binding to set the session for
+	CallOpts     bind.CallOpts     // Call options to use throughout this session
+	TransactOpts bind.TransactOpts // Transaction auth options to use throughout this session
+}
+
+// ShroomscoutsCallerSession is an auto generated read-only Go binding around an Ethereum contract,
+// with pre-set call options.
+type ShroomscoutsCallerSession struct {
+	Contract *ShroomscoutsCaller // Generic contract caller binding to set the session for
+	CallOpts bind.CallOpts       // Call options to use throughout this session
+}
+
+// ShroomscoutsTransactorSession is an auto generated write-only Go binding around an Ethereum contract,
+// with pre-set transact options.
+type ShroomscoutsTransactorSession struct {
+	Contract     *ShroomscoutsTransactor // Generic contract transactor binding to set the session for
+	TransactOpts bind.TransactOpts       // Transaction auth options to use throughout this session
+}
+
+// ShroomscoutsRaw is an auto generated low-level Go binding around an Ethereum contract.
+type ShroomscoutsRaw struct {
+	Contract *Shroomscouts // Generic contract binding to access the raw methods on
+}
+
+// ShroomscoutsCallerRaw is an auto generated low-level read-only Go binding around an Ethereum contract.
+type ShroomscoutsCallerRaw struct {
+	Contract *ShroomscoutsCaller // Generic read-only contract binding to access the raw methods on
+}
+
+// ShroomscoutsTransactorRaw is an auto generated low-level write-only Go binding around an Ethereum contract.
+type ShroomscoutsTransactorRaw struct {
+	Contract *ShroomscoutsTransactor // Generic write-only contract binding to access the raw methods on
+}
+
+// NewShroomscouts creates a new instance of Shroomscouts, bound to a specific deployed contract.
+func NewShroomscouts(address common.Address, backend bind.ContractBackend) (*Shroomscouts, error) {
+	contract, err := bindShroomscouts(address, backend, backend, backend)
+	if err != nil {
+		return nil, err
+	}
+	return &Shroomscouts{ShroomscoutsCaller: ShroomscoutsCaller{contract: contract}, ShroomscoutsTransactor: ShroomscoutsTransactor{contract: contract}, ShroomscoutsFilterer: ShroomscoutsFilterer{contract: contract}}, nil
+}
+
+// NewShroomscoutsCaller creates a new read-only instance of Shroomscouts, bound to a specific deployed contract.
+func NewShroomscoutsCaller(address common.Address, caller bind.ContractCaller) (*ShroomscoutsCaller, error) {
+	contract, err := bindShroomscouts(address, caller, nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &ShroomscoutsCaller{contract: contract}, nil
+}
+
+// NewShroomscoutsTransactor creates a new write-only instance of Shroomscouts, bound to a specific deployed contract.
+func NewShroomscoutsTransactor(address common.Address, transactor bind.ContractTransactor) (*ShroomscoutsTransactor, error) {
+	contract, err := bindShroomscouts(address, nil, transactor, nil)
+	if err != nil {
+		return nil, err
+	}
+	return &ShroomscoutsTransactor{contract: contract}, nil
+}
+
+// NewShroomscoutsFilterer creates a new log filterer instance of Shroomscouts, bound to a specific deployed contract.
+func NewShroomscoutsFilterer(address common.Address, filterer bind.ContractFilterer) (*ShroomscoutsFilterer, error) {
+	contract, err := bindShroomscouts(address, nil, nil, filterer)
+	if err != nil {
+		return nil, err
+	}
+	return &ShroomscoutsFilterer{contract: contract}, nil
+}
+
+// bindShroomscouts binds a generic wrapper to an already deployed contract.
+func bindShroomscouts(address common.Address, caller bind.ContractCaller, transactor bind.ContractTransactor, filterer bind.ContractFilterer) (*bind.BoundContract, error) {
+	parsed, err := abi.JSON(strings.NewReader(ShroomscoutsABI))
+	if err != nil {
+		return nil, err
+	}
+	return bind.NewBoundContract(address, parsed, caller, transactor, filterer), nil
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Shroomscouts *ShroomscoutsRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Shroomscouts.Contract.ShroomscoutsCaller.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Shroomscouts *ShroomscoutsRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Shroomscouts.Contract.ShroomscoutsTransactor.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Shroomscouts *ShroomscoutsRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Shroomscouts.Contract.ShroomscoutsTransactor.contract.Transact(opts, method, params...)
+}
+
+// Call invokes the (constant) contract method with params as input values and
+// sets the output to result. The result type might be a single field for simple
+// returns, a slice of interfaces for anonymous returns and a struct for named
+// returns.
+func (_Shroomscouts *ShroomscoutsCallerRaw) Call(opts *bind.CallOpts, result *[]interface{}, method string, params ...interface{}) error {
+	return _Shroomscouts.Contract.contract.Call(opts, result, method, params...)
+}
+
+// Transfer initiates a plain transaction to move funds to the contract, calling
+// its default method if one is available.
+func (_Shroomscouts *ShroomscoutsTransactorRaw) Transfer(opts *bind.TransactOpts) (*types.Transaction, error) {
+	return _Shroomscouts.Contract.contract.Transfer(opts)
+}
+
+// Transact invokes the (paid) contract method with params as input values.
+func (_Shroomscouts *ShroomscoutsTransactorRaw) Transact(opts *bind.TransactOpts, method string, params ...interface{}) (*types.Transaction, error) {
+	return _Shroomscouts.Contract.contract.Transact(opts, method, params...)
+}
+
+// MintToken is a paid mutator transaction binding the contract method 0xc634d032.
+//
+// Solidity: function mintToken(uint256 amount) returns()
+func (_Shroomscouts *ShroomscoutsTransactor) MintToken(opts *bind.TransactOpts, amount *big.Int) (*types.Transaction, error) {
+	return _Shroomscouts.contract.Transact(opts, "mintToken", amount)
+}
+
+// MintToken is a paid mutator transaction binding the contract method 0xc634d032.
+//
+// Solidity: function mintToken(uint256 amount) returns()
+func (_Shroomscouts *ShroomscoutsSession) MintToken(amount *big.Int) (*types.Transaction, error) {
+	return _Shroomscouts.Contract.MintToken(&_Shroomscouts.TransactOpts, amount)
+}
+
+// MintToken is a paid mutator transaction binding the contract method 0xc634d032.
+//
+// Solidity: function mintToken(uint256 amount) returns()
+func (_Shroomscouts *ShroomscoutsTransactorSession) MintToken(amount *big.Int) (*types.Transaction, error) {
+	return _Shroomscouts.Contract.MintToken(&_Shroomscouts.TransactOpts, amount)
+}
--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"math"
 	"math/big"
 	"os"
-	"time"
 
 	"github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts"
 	"github.com/ethereum/go-ethereum/accounts/abi/bind"
 	"github.com/ethereum/go-ethereum/accounts/keystore"
 	"github.com/ethereum/go-ethereum/common"
@@ -24,6 +24,7 @@ import (
 
 	"github.com/popsicleposse/dotdotbot/config"
 	"github.com/popsicleposse/dotdotbot/contracts/junglefreaks"
+	"github.com/popsicleposse/dotdotbot/contracts/shroomscouts"
 	"github.com/popsicleposse/dotdotbot/model"
 )
 
@@ -99,7 +100,7 @@ func main() {
 	}
 
 	var (
-		mintContractAddress = common.HexToAddress(conf.MintContract)
+		mintContractAddress = common.HexToAddress("0x3A6f22b9D33D68FaB863bb5c17E9D31476DF9FEA")
 		// botContractAddress  = common.HexToAddress(conf.BotContract)
 
 		successfulMints = 0
@@ -112,187 +113,266 @@ func main() {
 		log.Fatalln(err)
 	}
 
-	mintContract, err := junglefreaks.NewJunglefreaks(mintContractAddress, client)
+	mintContract, err := shroomscouts.NewShroomscouts(mintContractAddress, client)
 
 	if err != nil {
 		// Could not create the contract, oh well
 		log.Fatalln(err)
 	}
 
-	name, err := mintContract.Name(nil)
-	// contractAbi, err := dotdotdots.DotdotdotsMetaData.GetAbi()
+	selectedAccount := keyStore.Accounts()[0]
+	msg := ethereum.CallMsg{
+		From: selectedAccount.Address,
+		To:   &mintContractAddress,
+		Data: hexutil.MustDecode("0xc634d0320000000000000000000000000000000000000000000000000000000000000003"),
+	}
+
+	fmt.Println(selectedAccount.Address.String())
+
+	nonce, err := client.PendingNonceAt(context.Background(), selectedAccount.Address)
 
 	if err != nil {
 		log.Fatalln(err)
 	}
+	gasEstimate, err := client.EstimateGas(context.Background(), msg)
 
-	// literally just want to run it as long as we can
-	for {
+	fmt.Println(gasEstimate)
 
-		selectedAccount := keyStore.Accounts()[0]
+	keyStore.Unlock(selectedAccount, conf.KeystoreConf.Password)
 
-		// prepare the account to send a transaction
-		keyStore.Unlock(selectedAccount, conf.KeystoreConf.Password)
-		// check if the sale is
-		activeSale, err := mintContract.SaleOpen(&bind.CallOpts{})
+	tx, err := mintContract.MintToken(&bind.TransactOpts{
+		Signer: func(a common.Address, tx *types.Transaction) (*types.Transaction, error) {
+			return keyStore.SignTx(selectedAccount, tx, chainId)
+		},
+		GasLimit: gasEstimate,
+		// NoSend:   true,
+		Nonce: big.NewInt(int64(nonce)),
+	}, big.NewInt(int64(3)))
 
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(tx.Hash().String())
+	}
+
+	keyStore.Lock(selectedAccount.Address)
+	// // literally just want to run it as long as we can
+	// for {
+
+	// 	// prepare the account to send a transaction
+
+	// 	// check if the sale is
+	// 	activeSale, err := mintContract.SaleOpen(&bind.CallOpts{})
+
+	// 	if err != nil {
+	// 		// print the error
+	// 		log.Println(err)
+	// 	} else if activeSale {
+	// 		var pendingTxns []model.QueuedTxn
+	// 		database.Where(&model.QueuedTxn{Pending: true}).Find(&pendingTxns)
+
+	// 		var newTxns []common.Hash
+	// 		for _, txn := range pendingTxns {
+	// 			tx, pending, err := client.TransactionByHash(context.Background(), common.HexToHash(txn.Hash))
+
+	// 			if err != nil {
+	// 				log.Println(err)
+	// 			} else if pending {
+
+	// 				if err != nil {
+	// 					log.Println(err)
+	// 				} else {
+	// 					tx, err := mintContract.Mint(&bind.TransactOpts{
+	// 						Value: tx.Value(),
+	// 						Signer: func(a common.Address, tx *types.Transaction) (*types.Transaction, error) {
+	// 							return keyStore.SignTx(selectedAccount, tx, chainId)
+	// 						},
+	// 						GasLimit: gasEstimate * conf.Mint.GasMultiplier,
+	// 						Nonce:    big.NewInt(int64(txn.Nonce)),
+	// 					}, big.NewInt(int64(txn.Amount)))
+
+	// 					if err != nil {
+	// 						log.Println(err)
+	// 					} else {
+
+	// 						newTxns = append(newTxns, tx.Hash())
+	// 						// transaction no longer pending, update it.
+	// 						database.Where("hash = ?", txn.Hash).Delete(&txn)
+
+	// 						fmt.Println("successfully replayed the transaction, new txn hash:", tx.Hash().String())
+	// 					}
+
+	// 				}
+
+	// 			} else {
+	// 				// transaction no longer pending, update it.
+	// 				database.Where("hash = ?", txn.Hash).Delete(&txn)
+	// 			}
+	// 		}
+
+	// 		for _, txnHash := range newTxns {
+	// 			receipt, err := client.TransactionReceipt(context.Background(), txnHash)
+
+	// 			for err != nil {
+	// 				time.Sleep(500 * time.Millisecond)
+
+	// 				log.Println("no receipt yet. retrying.")
+	// 				receipt, err = client.TransactionReceipt(context.Background(), txnHash)
+	// 			}
+
+	// 			txn, pending, err := client.TransactionByHash(context.Background(), txnHash)
+
+	// 			for err != nil || pending {
+	// 				time.Sleep(500 * time.Millisecond)
+	// 				log.Println("no log yet. retrying.")
+	// 				txn, pending, err = client.TransactionByHash(context.Background(), txnHash)
+	// 			}
+
+	// 			database.Create(
+	// 				&model.FinalTxn{
+	// 					ProjectName: name,
+	// 					Status:      uint(receipt.Status),
+	// 					Hash:        txnHash.String()[2:],
+	// 					Value:       txn.Value().String(),
+	// 					Gas:         txn.Gas(),
+	// 					GasPrice:    txn.GasPrice().String(),
+	// 					Cost:        txn.Cost().String(),
+	// 				})
+	// 		}
+
+	// 		break
+
+	// 	} else {
+	// 		// if the sale is not active we want to place a transaction that will remain in pending util we update it.
+
+	// 		target := conf.Mint.MintTarget
+	// 		perTransaction := conf.Mint.MintPerTransaction
+
+	// 		transactionCount := int(math.Ceil(float64(target) / float64(perTransaction)))
+
+	// 		var queuedTxns []model.QueuedTxn
+
+	// 		database.Where(&model.QueuedTxn{}).Find(&queuedTxns)
+
+	// 		if len(queuedTxns) < int(transactionCount) {
+	// 			log.Println("queueing", transactionCount, "transactions")
+
+	// 			queueTxns(name, mintContract, client,
+	// 				selectedAccount, chainId,
+	// 				target, uint64(transactionCount), perTransaction, uint64(len(queuedTxns)))
+	// 			// totalQueued := 0
+	// 			// for i := 0; i < transactionCount; i++ {
+
+	// 			// 	mintCount := int(math.Min(float64((perTransaction)), float64(target-uint64(totalQueued))))
+	// 			// 	// grab the latest nonce we should be using...this is the number of pending transactions or simply the next
+	// 			// 	// nonce (final transaction count + 1)
+	// 			// 	nonce, err := client.PendingNonceAt(context.Background(), selectedAccount.Address)
+	// 			// 	if err != nil {
+	// 			// 		log.Println(err)
+	// 			// 	} else {
+	// 			// 		// calculate the value
+	// 			// 		value := ethToWei(conf.Mint.Price * float64(mintCount))
+
+	// 			// 		transaction, err := mintContract.Mint(&bind.TransactOpts{
+	// 			// 			Value:    value,
+	// 			// 			GasPrice: big.NewInt(8), // set to a low gas price
+	// 			// 			GasLimit: 530000,        // set to a minimum
+	// 			// 			Signer: func(a common.Address, tx *types.Transaction) (*types.Transaction, error) {
+	// 			// 				return keyStore.SignTx(selectedAccount, tx, chainId)
+	// 			// 			},
+	// 			// 			// NoSend: true,
+	// 			// 			Nonce: big.NewInt(int64(nonce)),
+	// 			// 		}, big.NewInt(int64(mintCount)))
+
+	// 			// 		if err != nil {
+	// 			// 			log.Println(err)
+	// 			// 		} else {
+	// 			// 			dbtx := &model.QueuedTxn{
+	// 			// 				ProjectName: name,
+	// 			// 				Hash:        transaction.Hash().String()[2:],
+	// 			// 				Pending:     true,
+	// 			// 				Status:      0,
+	// 			// 				Data:        hexutil.Encode(transaction.Data()),
+	// 			// 				Value:       transaction.Value().String(),
+	// 			// 				Amount:      uint64(mintCount),
+	// 			// 				Nonce:       transaction.Nonce(),
+	// 			// 			}
+
+	// 			// 			totalQueued += mintCount
+
+	// 			// 			database.Create(dbtx)
+	// 			// 		}
+
+	// 			// 	}
+	// 			// }
+
+	// 			log.Println("successfully queued", transactionCount, "transactions...awaiting mint.")
+	// 		} else {
+
+	// 			// queueTxns(name, mintContract, client,
+	// 			// 	selectedAccount, chainId,
+	// 			// 	target, uint64(transactionCount), perTransaction, uint64(len(queuedTxns)))
+
+	// 			log.Println(len(queuedTxns), "in queue...awaiting sale.")
+	// 			time.Sleep(500 * time.Millisecond)
+	// 		}
+
+	// 	}
+
+	// 	keyStore.Lock(keyStore.Accounts()[0].Address) // relock the account
+	// }
+
+	log.Printf("successfully minted %d NFT(s). target reached. shutting down\n", successfulMints)
+}
+
+// lol huge fucking mess...clean up for sure
+func queueTxns(name string, mintContract *junglefreaks.Junglefreaks, client *ethclient.Client,
+	selectedAccount accounts.Account, chainId *big.Int,
+	target, transactionCount, perTransaction, totalQueued uint64) {
+
+	for i := 0; i < int(transactionCount); i++ {
+
+		mintCount := uint64(math.Min(float64((perTransaction)), float64(target-uint64(totalQueued))))
+		// grab the latest nonce we should be using...this is the number of pending transactions or simply the next
+		// nonce (final transaction count + 1)
+		nonce, err := client.PendingNonceAt(context.Background(), selectedAccount.Address)
 		if err != nil {
-			// print the error
 			log.Println(err)
-		} else if activeSale {
-			var pendingTxns []model.QueuedTxn
-			database.Where(&model.QueuedTxn{Pending: true}).Find(&pendingTxns)
-
-			var newTxns []common.Hash
-			for _, txn := range pendingTxns {
-				tx, pending, err := client.TransactionByHash(context.Background(), common.HexToHash(txn.Hash))
-
-				if err != nil {
-					log.Println(err)
-				} else if pending {
-
-					msg := ethereum.CallMsg{
-						From:  selectedAccount.Address,
-						To:    &mintContractAddress,
-						Value: tx.Value(),
-						Data:  tx.Data(),
-					}
-					gasEstimate, err := client.EstimateGas(context.Background(), msg)
-
-					if err != nil {
-						log.Println(err)
-					} else {
-						tx, err := mintContract.Mint(&bind.TransactOpts{
-							Value: tx.Value(),
-							Signer: func(a common.Address, tx *types.Transaction) (*types.Transaction, error) {
-								return keyStore.SignTx(selectedAccount, tx, chainId)
-							},
-							GasLimit: gasEstimate * conf.Mint.GasMultiplier,
-							Nonce:    big.NewInt(int64(txn.Nonce)),
-						}, big.NewInt(int64(txn.Amount)))
-
-						if err != nil {
-							log.Println(err)
-						} else {
-
-							newTxns = append(newTxns, tx.Hash())
-							// transaction no longer pending, update it.
-							database.Where("hash = ?", txn.Hash).Delete(&txn)
-
-							fmt.Println("successfully replayed the transaction, new txn hash:", tx.Hash().String())
-						}
-
-					}
-
-				} else {
-					// transaction no longer pending, update it.
-					database.Where("hash = ?", txn.Hash).Delete(&txn)
-				}
-			}
-
-			for _, txnHash := range newTxns {
-				receipt, err := client.TransactionReceipt(context.Background(), txnHash)
-
-				for err != nil {
-					time.Sleep(500 * time.Millisecond)
-
-					log.Println("no receipt yet. retrying.")
-					receipt, err = client.TransactionReceipt(context.Background(), txnHash)
-				}
-
-				txn, pending, err := client.TransactionByHash(context.Background(), txnHash)
-
-				for err != nil || pending {
-					time.Sleep(500 * time.Millisecond)
-					log.Println("no log yet. retrying.")
-					txn, pending, err = client.TransactionByHash(context.Background(), txnHash)
-				}
-
-				database.Create(
-					&model.FinalTxn{
-						ProjectName: name,
-						Status:      uint(receipt.Status),
-						Hash:        txnHash.String()[2:],
-						Value:       txn.Value().String(),
-						Gas:         txn.Gas(),
-						GasPrice:    txn.GasPrice().String(),
-						Cost:        txn.Cost().String(),
-					})
-			}
-
-			break
-
 		} else {
-			// if the sale is not active we want to place a transaction that will remain in pending util we update it.
+			// calculate the value
+			value := ethToWei(conf.Mint.Price * float64(mintCount))
 
-			target := conf.Mint.MintTarget
-			perTransaction := conf.Mint.MintPerTransaction
+			transaction, err := mintContract.Mint(&bind.TransactOpts{
+				Value:    value,
+				GasPrice: big.NewInt(8), // set to a low gas price
+				GasLimit: 530000,        // set to a minimum
+				Signer: func(a common.Address, tx *types.Transaction) (*types.Transaction, error) {
+					return keyStore.SignTx(selectedAccount, tx, chainId)
+				},
+				// NoSend: true,
+				Nonce: big.NewInt(int64(nonce)),
+			}, big.NewInt(int64(mintCount)))
 
-			transactionCount := int(math.Ceil(float64(target) / float64(perTransaction)))
-
-			var queuedTxns []model.QueuedTxn
-
-			database.Where(&model.QueuedTxn{}).Find(&queuedTxns)
-
-			if len(queuedTxns) < int(transactionCount) {
-				log.Println("queueing", transactionCount, "transactions")
-
-				totalQueued := 0
-				for i := 0; i < transactionCount; i++ {
-
-					mintCount := int(math.Min(float64((perTransaction)), float64(target-uint64(totalQueued))))
-					// grab the latest nonce we should be using...this is the number of pending transactions or simply the next
-					// nonce (final transaction count + 1)
-					nonce, err := client.PendingNonceAt(context.Background(), selectedAccount.Address)
-					if err != nil {
-						log.Println(err)
-					} else {
-						// calculate the value
-						value := ethToWei(conf.Mint.Price * float64(mintCount))
-
-						transaction, err := mintContract.Mint(&bind.TransactOpts{
-							Value:    value,
-							GasPrice: big.NewInt(8), // set to a low gas price
-							GasLimit: 530000,        // set to a minimum
-							Signer: func(a common.Address, tx *types.Transaction) (*types.Transaction, error) {
-								return keyStore.SignTx(selectedAccount, tx, chainId)
-							},
-							// NoSend: true,
-							Nonce: big.NewInt(int64(nonce)),
-						}, big.NewInt(int64(mintCount)))
-
-						if err != nil {
-							log.Println(err)
-						} else {
-							dbtx := &model.QueuedTxn{
-								ProjectName: name,
-								Hash:        transaction.Hash().String()[2:],
-								Pending:     true,
-								Status:      0,
-								Data:        hexutil.Encode(transaction.Data()),
-								Value:       transaction.Value().String(),
-								Amount:      uint64(mintCount),
-								Nonce:       transaction.Nonce(),
-							}
-
-							totalQueued += mintCount
-
-							database.Create(dbtx)
-						}
-
-					}
+			if err != nil {
+				log.Println(err)
+			} else {
+				dbtx := &model.QueuedTxn{
+					ProjectName: name,
+					Hash:        transaction.Hash().String()[2:],
+					Pending:     true,
+					Status:      0,
+					Data:        hexutil.Encode(transaction.Data()),
+					Value:       transaction.Value().String(),
+					Amount:      uint64(mintCount),
+					Nonce:       transaction.Nonce(),
 				}
 
-				log.Println("successfully queued", transactionCount, "transactions...awaiting mint.")
-			} else {
-				log.Println(len(queuedTxns), "in queue...awaiting sale.")
-				time.Sleep(500 * time.Millisecond)
+				totalQueued += mintCount
+
+				database.Create(dbtx)
 			}
 
 		}
-
-		keyStore.Lock(keyStore.Accounts()[0].Address) // relock the account
 	}
 
-	log.Printf("successfully minted %d NFT(s). target reached. shutting down\n", successfulMints)
 }
